daa57228ca52290c805a6dcfc203807fb24777ee
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-04-24 17:38:43 +0400

    Implemented twitter call on tech-news page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
+
   resources :blogs do
     member do
       get :toggle_status
