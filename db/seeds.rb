edbd4b3bae94cac7f0d186b19bc186e8c654f34d
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-04-27 21:18:11 +0400

    Small changes after about me page problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "password",
+  password_confirmation: "password",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "password",
+  password_confirmation: "password",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
@@ -16,7 +35,7 @@ end
 
 puts "10 blog posts created"
 
-5.times do |skill|
+3.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
     percent_utilized: 15
