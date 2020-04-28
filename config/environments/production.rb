3d8bb5a8c40c8c9f2b604a9f4dec2975e2050e1d
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-04-28 15:58:49 +0400

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://tsotne-ok.herokuapp.com', 'http://tsotne-ok.herokuapp.com']
+  config.action_cable.url = "wss://tsotne-ok.herokuapp.com/cable"
 end
